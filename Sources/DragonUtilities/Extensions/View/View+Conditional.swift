0985bf22f8841fac6c9d72f0b903312467282518
Thimo Bess <thimo.bess@gmail.com>
2024-07-06 17:39:25 +0200

    documentation: Add some comments

--- Sources/DragonUtilities/Extensions/View/View+Conditional.swift
+++ Sources/DragonUtilities/Extensions/View/View+Conditional.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 public extension View {
+    // A function that conditionally applies a transformation to a view.
+    // - Parameters:
+    //   - condition: A Boolean value that determines whether the transformation is applied.
+    //   - transform: A closure that takes the current view and returns a transformed view.
+    // - Returns: Either the transformed view if the condition is true, or the original view if the condition is false.
     @ViewBuilder
     func ifCondition(_ condition: Bool, transform: (Self) -> some View) -> some View {
         if condition {
@@ -17,6 +22,14 @@ public extension View {
         }
     }
 
+    // A function that conditionally applies one of two transformations to a view.
+    // - Parameters:
+    //   - condition: A Boolean value that determines which transformation is applied.
+    //   - transform: A closure that takes the current view and returns a transformed view if the condition is true.
+    //   - elseTransform: A closure that takes the current view and returns a transformed view if the condition is
+    //   false.
+    // - Returns: Either the transformed view if the condition is true, or the elseTransformed view if the condition is
+    // false.
     @ViewBuilder
     func ifElse(_ condition: Bool, transform: (Self) -> some View, elseTransform: (Self) -> some View) -> some View {
         if condition {
